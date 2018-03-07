de423cd33c45fa2c8f50b7819a8be86c72747733
agnynureza <rezaagny@gmail.com>
2018-03-07 07:18:13 +0700

    release 3

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -2,19 +2,19 @@ const Transaction = require('../models/transaction')
 
 module.exports = {
     addTransaction(req,res){
-        const {member,days,fine,booklist} = req.body
+        const {member,days,fine,in_date,booklist} = req.body
         const transaction = new Transaction(
             {member,
             days,
             out_date:new Date(),
-            due_date:new Date(),
-            in_date: new Date(),
+            due_date:new Date().setDate(new Date().getDate() + Number(req.body.days)),
+            in_date,
             fine,
             booklist}
         )
-        Transaction
+        transaction
         .save((err,r)=>{
-            if(err) throw new error('invalid add Transaction');
+            if(err) return res.status(500).send(err);
             res.status(201).send({
                 message : 'add Transaction success',
                 data : r
@@ -24,8 +24,8 @@ module.exports = {
     showTransaction(req,res){
         Transaction
         .find()
-        .populate('Book')
-        .populate('Customer')
+        .populate('member')
+        .populate('booklist')
         .exec()
         .then(data=>{
             res.status(201).send({
@@ -47,8 +47,14 @@ module.exports = {
     },
     updateTransaction(req,res){
      const id = req.params.id
+    //  const diff = Transaction.due_date - new Date(req.body.in_date) 
      Transaction
-     .update({_id:id},{$set:req.body})
+     .update({_id:id},{
+         $set:{
+             in_date:new Date(req.body.in_date)
+            //  fine:diff*1000
+            }
+        })
      .then(data=>{
          res.status(201).send({
              message : 'update customer success',
